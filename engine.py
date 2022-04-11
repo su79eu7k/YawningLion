83ce2de45798d101e28f7ec880615c5a4f9b3dd8
sekim <sungeun.kim@zf.com>
2022-04-11 15:13:33 +0900

    get selection

--- engine.py
+++ engine.py
@@ -3,26 +3,26 @@ import openpyxl as ox
 import xlwings as xw
 
 
-def ox_load_workbook(filepath):
-    _path = '/'.join(filepath.split('/')[:-1])
-    _name = filepath.split('/')[-1]
-
-    os.chdir(_path)
-
-    wb = ox.load_workbook(_name)
-    ws = wb['Sheet1']
-    ws['A1'] = 'test'
-    wb.save(_name)
-
-    return True
-
-
 def xw_load_workbooks(filepath):
     return xw.Book(filepath)
 
 
+def xw_activate_workbook(func):
+    def wrapper(workbook_to_activate):
+        workbook_to_activate.activate()
+        return func(workbook_to_activate)
+    return wrapper
+
+
+@xw_activate_workbook
+def xw_get_selection(workbook):
+    return workbook.selection.get_address(False, False, False, False)
+
+
 if __name__ == '__main__':
-    test_file = 'D:/Localhome/sekim/OneDrive - ZF Friedrichshafen AG/Desktop/NPV concept B.xlsx'
+    test_file = 'D:/Localhome/sekim/OneDrive - ZF Friedrichshafen AG/Desktop/NPV concept.xlsx'
     # ox_load_workbook(test_file)
 
-    print(xw_load_workbooks(test_file))
+    wb = xw_load_workbooks(test_file)
+    print(xw_get_selection(wb))
+
--- server.py
+++ server.py
@@ -6,23 +6,38 @@ from pydantic import BaseModel
 class Worker:
     def __init__(self):
         self.filepath = None
-        self.wb = None
+        self.workbook = None
+        self.selection = None
 
     def connect_workbook(self, filepath):
         try:
-            self.wb = eng.xw_load_workbooks(filepath)
+            self.workbook = eng.xw_load_workbooks(filepath)
 
             return True
         except FileNotFoundError as ex:
 
             return False
 
+    def get_selection(self):
+        self.selection = eng.xw_get_selection(self.workbook)
+
+        return True
+
 
 sess = Worker()
 
 
+class Message(BaseModel):
+    code: int = 9
+    message: str | None = None
+
+
 class FileInfo(BaseModel):
     filepath: str
+
+
+class Selection(Message):
+    address: str
 
 
 app = FastAPI()
@@ -33,9 +48,24 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/connect_workbook")
+@app.post("/connect_workbook", response_model=Message)
 async def connect_workbook(fileinfo: FileInfo):
-    if sess.connect_workbook(fileinfo.filepath):
-        return {"message": "Success"}
+    # if sess.connect_workbook(fileinfo.filepath):
+    if sess.connect_workbook('D:/Localhome/sekim/OneDrive - ZF Friedrichshafen AG/Desktop/NPV concept.xlsx'):
+        return {"code": 1, "message": "Success"}
     else:
-        return {"message": "Failed"}
+        return {"code": 0, "message": "Failed"}
+
+
+@app.get("/get_selection", response_model=Selection)
+async def get_selection():
+    if sess.workbook:
+        sess.get_selection()
+        if sess.selection:
+            if ':' in sess.selection:
+                return {"address": "Range", "code": 0, "message": "Failed: Selection too wide"}
+            else:
+                return {"address": sess.selection, "code": 1, "message": "Success"}
+        else:
+            return {"address": "None", "code": 0, "message": "Failed: No selection"}
+
